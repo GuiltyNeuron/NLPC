e44aea0fe8dc26d047da8a880ac4387de7eb18e3
Achraf KHAZRI <A.khazri@openbee.com>
2019-09-08 14:41:42 +0100

    :tada: :construction: Embedding class intial commit

---
+++ src/Embedding.cpp
@@ -0,0 +1,15 @@
+// Project : NLP ++
+// Author : Achraf KHAZRI AI Research Engineer
+// Script : Embedding.cpp
+// Description :
+
+
+#include "Embedding.h"
+
+Embedding::Embedding()
+{
+}
+
+Embedding::~Embedding()
+{
+}
---
+++ src/Embedding.h
@@ -0,0 +1,17 @@
+// Project : NLP ++
+// Author : Achraf KHAZRI AI Research Engineer
+// Script : Embedding.h
+// Description : 
+
+#include <iostream>
+
+using namespace std;
+class Embedding
+{
+public:
+	Embedding();
+	~Embedding();
+
+private:
+
+};
