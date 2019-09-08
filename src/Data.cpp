60cc82c97789685cae4b8afb9587d984f7917f02
Achraf KHAZRI <A.khazri@openbee.com>
2019-09-08 13:02:14 +0100

    :sparkles :construction: Adding Data class

---
+++ src/Data.cpp
@@ -0,0 +1,29 @@
+// Project : NLP ++
+// Author : Achraf KHAZRI AI Research Engineer
+// Script : Data.cpp
+// Description :
+
+#include "Data.h"
+
+string Data::readTxt(string inputFilePath)
+{
+	return "";
+}
+
+string Data::readPdf(string inputFilePath)
+{
+	return "";
+}
+
+string Data::readCsv(string inputFilePath)
+{
+	return "";
+}
+
+Data::Data()
+{
+}
+
+Data::~Data()
+{
+}
---
+++ src/Data.h
@@ -0,0 +1,24 @@
+// Project : NLP ++
+// Author : Achraf KHAZRI AI Research Engineer
+// Script : Data.h
+// Description : 
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Data
+{
+public:
+	Data();
+	~Data();
+	string readTxt(string);
+	string readPdf(string);
+	string readCsv(string);
+	
+
+private:
+
+};
